996d07cf4410cbe704f6305cf5514cb162dc78f5
Indravesh Anand <indravesh11@gmail.com>
2018-10-12 14:28:38 +0530

    corrected flaw in validates method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
   validate :password_rule
   validates :password, length: {maximum: 12, minimum: 6}
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
   def password_rule
 	  message = "must include on lowercase, one uppercase, a number and a special char"
-	  if not email.match(/[a-z]/).present?
+	  if not password.match(/[a-z]/).present?
             errors.add(:base, message)
-	  elsif not email.match(/[A-Z]/).present?
+	  elsif not password.match(/[A-Z]/).present?
 		  errors.add(:base, message)
-	  elsif not email.match(/[0-9]/).present?	
+	  elsif not password.match(/[0-9]/).present?	
 		  errors.add(:base, message)
-          elsif not email.match(/[!-+]/).present?		  
+          elsif not password.match(/[!-+]|(@|^)/).present?		  
 		  errors.add(:base, message)
 	  end		  
           		  
