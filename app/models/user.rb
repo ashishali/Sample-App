a12782c695e7debf09c7a214f117a27bdceb5053
Indravesh Anand <indravesh11@gmail.com>
2018-10-12 14:02:48 +0530

    validates user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  validate :password_rule
+  validates :password, length: {maximum: 12, minimum: 6}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  def password_rule
+	  message = "must include on lowercase, one uppercase, a number and a special char"
+	  if not email.match(/[a-z]/).present?
+            errors.add(:base, message)
+	  elsif not email.match(/[A-Z]/).present?
+		  errors.add(:base, message)
+	  elsif not email.match(/[0-9]/).present?	
+		  errors.add(:base, message)
+          elsif not email.match(/[!-+]/).present?		  
+		  errors.add(:base, message)
+	  end		  
+          		  
+  end
 end
